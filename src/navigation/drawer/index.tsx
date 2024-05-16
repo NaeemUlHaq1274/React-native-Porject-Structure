d1d280e489cda67769655e17b3b56cd113226ddb
Naeemul_Haq <134818197+NaeemUlHaq1274@users.noreply.github.com>
2024-05-16 10:57:22 +0500

    SOME UPDATES

--- src/navigation/drawer/index.tsx
+++ src/navigation/drawer/index.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerContent from './DrawerContent';
 import { HomeScreen, SubscriptionScreen } from '@screens';
+import { SCREEN_NAMES } from '@constants';
 
 const Drawer = createDrawerNavigator();
 
@@ -10,8 +11,8 @@ const DrawerNavigation: React.FC = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
-        <Drawer.Screen name="HOME_SCREEN" component={HomeScreen} />
-        <Drawer.Screen name="SETTING_SCREEN" component={SubscriptionScreen} />
+        <Drawer.Screen name={SCREEN_NAMES.HOME_SCREEN} component={HomeScreen} />
+        <Drawer.Screen name={SCREEN_NAMES.SUBSCRIPTION_SCREEN} component={SubscriptionScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
